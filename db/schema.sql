fcf0db8fc880ab366930e2cd2db56b7b75e6e9ae
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-22 15:18:54 -0700

    added role table.

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
