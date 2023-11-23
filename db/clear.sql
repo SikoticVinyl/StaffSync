6014199ccbfbd02cae495bc82239cb31050b30af
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-22 19:31:48 -0700

    added ability to clear database via clear.sql.

---
+++ db/clear.sql
@@ -0,0 +1,9 @@
+-- Used in development to clear database from test added info.
+-- Delete all records from the 'department' table
+DELETE FROM department;
+
+-- Delete all records from the 'role' table
+DELETE FROM role;
+
+-- Delete all records from the 'employee' table
+DELETE FROM employee;
