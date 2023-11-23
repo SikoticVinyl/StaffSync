ba26a582b8749a749aa8c47ed843ba3439cf64b9
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-22 19:51:28 -0700

    updated Schema.sql to reset data for dev purposes, got add department working.

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,10 @@ CREATE DATABASE IF NOT EXISTS employee_db;
 
 USE employee_db;
 
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
